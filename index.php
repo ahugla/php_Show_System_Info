e9da46dd487b27b2b8762a39d5cfff2023b2f07a
ahugla <ahugla@vmware.com>
2016-12-20 11:14:09 +0100

    Adding code

--- index.php
+++ index.php
@@ -11,7 +11,27 @@ and open the template in the editor.
     </head>
     <body>
         <?php
-        // put your code here
+            echo "<font size='18' face='Arial'>";
+            echo "LOAD BALANCING TEST".'</br>';
+            echo " ".'</br>';
+            echo " ".'</br>';
+            
+            echo "<font size=9' face='Arial'>";
+            echo "server name = " .gethostname().'</br>';
+            echo "IP address = " .getHostByName(getHostName()).'</br>';
+
+            echo " ".'</br>';
+            echo " ".'</br>';
+            echo " ".'</br>';
+            echo " ".'</br>';
+            echo " ".'</br>';
+            echo " ".'</br>';
+            echo " ".'</br>';
+            echo " ".'</br>';
+            
+            echo "<font size=5' face='Arial'>";
+            echo "v 1.0".'</br>';
+            echo "20 Dec 2016";
         ?>
     </body>
 </html>
